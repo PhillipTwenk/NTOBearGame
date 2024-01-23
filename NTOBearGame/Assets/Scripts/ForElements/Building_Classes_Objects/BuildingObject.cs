4a15629d8f41ba35a5775a2d2de72df6a6fc9653
Artefok <Artefok2017@mail.ru>
2024-01-23 16:26:22 +0700

    - 2 algorithms max

--- NTOBearGame/Assets/Scripts/ForElements/Building_Classes_Objects/BuildingObject.cs
+++ NTOBearGame/Assets/Scripts/ForElements/Building_Classes_Objects/BuildingObject.cs
@@ -28,15 +28,15 @@ public class BuildingObject : MonoBehaviour
     // Переменные
     [SerializeField] string building_name; // Название агрегата в интерфейсе (рус., задаётся в каждом агрегате своё значение из БД)
     [SerializeField] string sys_building_name; // Название агрегата для операций внутри (англ., задаётся в каждом агрегате своё значение из БД)
-    private string action; // Действие алгоритма
-    private string exit; // Конечная точка алгоритма, куда отправить итог (если указан текущий агрегат - вывод сразу рядом с ним)
+    private List<string> actions = new List<string>(){}; // Действие алгоритма
+    private List<string> exits = new List<string>(){}; // Конечная точка алгоритма, куда отправить итог (если указан текущий агрегат - вывод сразу рядом с ним)
     private string reaction_state; // Статус реакции
-    private int parameter = 0; // Параметр алгоритма
+    private List<int> parameters = new List<int>(){}; // Параметр алгоритма
     private float timer;
     private bool is_canvas_activated = false; // Состояние канваса 
     private bool is_reacted = false;
-    public List<int> element_ids; // Используемые в алгоритме ID элементов из базы (если 0 -> вещества нет)
-    public List<string> element_names; // Используемые в алгоритме имена веществ из базы (если "" -> вещества нет)
+    public List<List<int>> element_ids = new List<List<int>>(){}; // Используемые в алгоритме ID элементов из базы (если 0 -> вещества нет)
+    public List<List<string>> element_names = new List<List<string>>(){}; // Используемые в алгоритме имена веществ из базы (если "" -> вещества нет)
     public List<string> temp_storage; // Хранилище имён элементов, попавших в агрегат 
     public List<int> temp_element_ids; // Хранилище ID элементов, попавших в агрегат 
     public List<string> elements_info; // Список всех элементов, подающихся на выбор в агрегате
@@ -63,9 +63,7 @@ public class BuildingObject : MonoBehaviour
     {
         Transporter.AgregatInputPlaces[building_name] = InputPlace;
         // обнуляем сохраненный алгоритм до этого
-        DeleteAlgorithm();
         AgregatName.text = $"{building_name}";
-
         ExitAgregatUI();
     }
     public void ExitAgregatUI(){
@@ -81,8 +79,7 @@ public class BuildingObject : MonoBehaviour
     void Update()
     {
         if(is_canvas_activated && Input.GetKey(KeyCode.Escape)){ // нажимая Esc при открытом канвасе(UI агрегата) -> закрываем его с полем ввода параметра
-            CheckChosenAction(); // сохраняем в action текущее действие
-            if(action != "" && (ElementsChoice1.value != 0 && ElementsChoice2.value != 0) && ExitsChoice.options[ExitsChoice.value].text != ""){ // если алгоритм есть
+            if(ActionsChoice.value != 0 && (ElementsChoice1.value != 0 && ElementsChoice2.value != 0) && ExitsChoice.options[ExitsChoice.value].text != ""){ // если алгоритм есть
                 BuildAlgorithm(); // сохраняем алгоритм
             } else { // если же нет
                 ExitAgregatUI();
@@ -135,125 +132,142 @@ public class BuildingObject : MonoBehaviour
     // OUTPUT: - (сохранение во все нужные переменные, отключение интерфейса агрегата и включение меню игрока)
     public void BuildAlgorithm(){
         // нахождение нужных переменных для последующего сохранения алгоритма
-        element_ids.Clear();
-        element_names.Clear();
-
-        parameter = Convert.ToInt32(ParameterInput.text);
-        exit = ExitsChoice.options[ExitsChoice.value].text;
-        element_ids.Add(ElementsChoice1.value);
-        element_ids.Add(ElementsChoice2.value);
-        element_names.Add(ElementsChoice1.options[ElementsChoice1.value].text);
-        element_names.Add(ElementsChoice2.options[ElementsChoice2.value].text);
-
-        // сохраняем все части алгоритма в PlayerPrefs
-        for(int i = 1; i < 3; i++){
-            PlayerPrefs.SetInt($"{sys_building_name}ElementID{i}", element_ids[i-1]);
-            if(element_names[i-1] != ""){
-                PlayerPrefs.SetString($"{sys_building_name}ElementName{i}", element_names[i-1]);
-            } else {
-                PlayerPrefs.SetString($"{sys_building_name}ElementName{i}", "-");
-            }
+        if(element_ids.Count < 2){
+            Debug.Log(1);
+            parameters.Add(Convert.ToInt32(ParameterInput.text));
+            Debug.Log(2);
+            exits.Add(ExitsChoice.options[ExitsChoice.value].text);
+            Debug.Log(3);
+            actions.Add(ActionsChoice.options[ActionsChoice.value].text);
+            Debug.Log("--");
+            List<int> temp_elem_ids = new List<int>(){ElementsChoice1.value, ElementsChoice2.value};
+            Debug.Log(ElementsChoice1.value);
+            Debug.Log(ElementsChoice2.value);
+            element_ids.Add(temp_elem_ids);
+            Debug.Log("--");
+            List<string> temp_elem_names = new List<string>(){ElementsChoice1.options[ElementsChoice1.value].text, ElementsChoice2.options[ElementsChoice2.value].text};
+            Debug.Log(7);
+            Debug.Log(element_ids.Count);
+            element_names.Add(temp_elem_names);
+            Debug.Log(8);
+        } else {
+            Debug.Log("Максимум алгоритмов достигнут");
         }
-        PlayerPrefs.SetString($"{sys_building_name}Action", action);
-        PlayerPrefs.SetString($"{sys_building_name}Exit", exit);
-        PlayerPrefs.SetInt($"{sys_building_name}Parameter", parameter);
-        ParameterInput.text = ""; // сброс параметра в строке ввода
+        
+        // сохраняем все части алгоритма в PlayerPrefs
+        // for(int i = 1; i < 3; i++){
+        //     PlayerPrefs.SetInt($"{sys_building_name}ElementID{i}", element_ids[i-1]);
+        //     if(element_names[i-1] != ""){
+        //         PlayerPrefs.SetString($"{sys_building_name}ElementName{i}", element_names[i-1]);
+        //     } else {
+        //         PlayerPrefs.SetString($"{sys_building_name}ElementName{i}", "-");
+        //     }
+        // }
+        // PlayerPrefs.SetString($"{sys_building_name}Action", action);
+        // PlayerPrefs.SetString($"{sys_building_name}Exit", exit);
+        // PlayerPrefs.SetInt($"{sys_building_name}Parameter", parameter);
 
         // составление строки алгоритма 
-        if(element_ids[1] == 0){
-            AlgorithmText.text = $"{action} {element_names[0]}, Параметр = {parameter}, Вывести в {exit}";
+        if(element_ids[element_ids.Count-1][element_ids[element_ids.Count-1].Count-1] == 0){
+            Debug.Log(10);
+            AlgorithmText.text += $"\n{actions[actions.Count-1]} {element_names[element_names.Count-1][0]}, Параметр = {parameters[parameters.Count-1]}, Вывести в {exits[exits.Count-1]}";
+            ParameterInput.text = ""; // сброс параметра в строке ввода
+            ExitAgregatUI();
         } else {
-            AlgorithmText.text = $"{action} {element_names[0]} и {element_names[1]}, Параметр = {parameter}, Вывести в {exit}";
+            Debug.Log(11);
+            AlgorithmText.text += $"\n{actions[actions.Count-1]} {element_names[element_names.Count-1][0]} и {element_names[element_names.Count-1][1]}, Параметр = {parameters[parameters.Count-1]}, Вывести в {exits[exits.Count-1]}";
+            ParameterInput.text = ""; // сброс параметра в строке ввода
+            ExitAgregatUI();
         }
-
-        ExitAgregatUI();
-    }
-
-    // Сохранение действия при выборе
-    // INPUT: - (выход из выпадающего списка после выбора)
-    // OUTPUT: - (сохранение действия в action и в зависимости от него включение параметра)
-    public void CheckChosenAction(){ // проверка при выборе действия (всегда в одном порядке: 1 - с параметром; 2 - без параметра; 3 - удалить остатки)
-        action = ActionsChoice.options[ActionsChoice.value].text;
-    }
-
-    // Сохранение результата параметра
-    // INPUT: - (выход из строки ввода после её заполнения)
-    // OUTPUT: - (сохранение в параметр значение строки ввода)
-    public void ParameterInputResult(){
-        parameter = Convert.ToInt32(ParameterInput.text);
     }
 
     public void DeleteAlgorithm(){
-        for(int i = 1; i < 3; i++){
-            PlayerPrefs.DeleteKey($"{sys_building_name}ElementName{i}");
-            PlayerPrefs.DeleteKey($"{sys_building_name}ElementID{i}");
-        }
-
-        PlayerPrefs.DeleteKey($"{sys_building_name}Parameter");
-        PlayerPrefs.DeleteKey($"{sys_building_name}Action");
-        PlayerPrefs.DeleteKey($"{sys_building_name}Exit");
+        element_ids.RemoveAt(element_ids.Count-1);
+        element_names.RemoveAt(element_names.Count-1);
+        parameters.RemoveAt(parameters.Count-1);
+        actions.RemoveAt(actions.Count-1);
+        exits.RemoveAt(exits.Count-1);
         AlgorithmText.text = "";
-
-        parameter = 0;
-        action = "";
     }
 
     private void OnTriggerEnter(Collider coll){
         string element_name = coll.name.ToString().Split('(')[0]; // просмотр имени объекта (split сделан в случае такого названия - Na(Clone), которое наш алгоритм не засчитает за элемент впринципе)
-        if(!elements_info.Contains(coll.name) || !element_names.Contains(element_name)){ // если прикоснувшийся объект вообще не вещество или он не указан в списке элементов алгоритма
+        if(!elements_info.Contains(coll.name) && (!element_names[0].Contains(element_name) || !element_names[1].Contains(element_name))){ // если прикоснувшийся объект вообще не вещество или он не указан в списке элементов алгоритма
             return;
         }
 
         temp_storage.Add(element_name); // добавление элемента в хранилище агрегата
         Destroy(coll.gameObject); // моментальное уничтожение объекта (в данном контексте положили в агрегат)
         InputElementsText.text += $"{element_name}\n";
-
-        // Условие: если в temp_storage находятся вещества, которые являются условием для начала алгоритма, то начинается подготовка к проведению реакции
-        if((temp_storage.Contains(PlayerPrefs.GetString($"{sys_building_name}ElementName1")) || PlayerPrefs.GetString($"{sys_building_name}ElementName1") == "-") && (temp_storage.Contains(PlayerPrefs.GetString($"{sys_building_name}ElementName2")) || PlayerPrefs.GetString($"{sys_building_name}ElementName2") == "-") ){
+        string temp_action = "";
+        string temp_exit = "";
+        int temp_algorithm_id = -1;
+        List<Dictionary<string, string>> result_element = null;
+        // Условие: если в temp_storage находятся вещества, которые являются условием для начала алгоритма #1, то начинается подготовка к проведению реакции
+        if(temp_storage.Equals(element_names[0])){
+            temp_algorithm_id = 0;
             // добавление ID соответствующих элементов в temp_element_ids
-            temp_element_ids.Add(PlayerPrefs.GetInt($"{sys_building_name}ElementID1")); 
-            temp_element_ids.Add(PlayerPrefs.GetInt($"{sys_building_name}ElementID2"));
-
+            temp_element_ids.Add(element_ids[0][0]); 
+            temp_element_ids.Add(element_ids[0][1]);
+            temp_action = actions[0];
+            temp_exit = exits[0];
             // пример вызова функции для получения вещества по алгоритму (строение, действие, ID элементов, параметр)
-            List<Dictionary<string, string>> result_element = Building.Reaction( 
+            result_element = Building.Reaction( 
                 building_name, // строение
-                PlayerPrefs.GetString($"{sys_building_name}Action"), // действие
+                actions[0], // действие
                 temp_element_ids, // ID элементов в БД
-                parameter=PlayerPrefs.GetInt($"{sys_building_name}Parameter") // параметр действия
+                parameters[0] // параметр действия
             );
+        // Условие: если в temp_storage находятся вещества, которые являются условием для начала алгоритма #2, то начинается подготовка к проведению реакции
+        } else if (temp_storage.Equals(element_names[element_names.Count-1])){
+            temp_algorithm_id = 1;
+            // добавление ID соответствующих элементов в temp_element_ids
+            temp_element_ids.Add(element_ids[element_ids.Count-1][0]); 
+            temp_element_ids.Add(element_ids[element_ids.Count-1][1]);
+            temp_action = actions[actions.Count-1];
+            temp_exit = exits[exits.Count-1];
+            // пример вызова функции для получения вещества по алгоритму (строение, действие, ID элементов, параметр)
+            result_element = Building.Reaction( 
+                building_name, // строение
+                actions[actions.Count-1], // действие
+                temp_element_ids, // ID элементов в БД
+                parameters[parameters.Count-1] // параметр действия
+            );
+        }
 
-
-            // если результата нет
-            if(Convert.ToInt32(result_element[0]["element_id"]) == temp_element_ids[0] && PlayerPrefs.GetString($"{sys_building_name}Action") != "Аннигилировать, оставить основной элемент"){
-                reaction_state = "Нет такой реакции!"; // выставление соответствующего состояния агрегата
-                if(exit == building_name){
-                    Instantiate(EP.elements_prefabs[Convert.ToInt32(result_element[0]["element_id"])-1], OutputPlace.transform.position, Quaternion.identity);
-                } else {
-                    Instantiate(EP.elements_prefabs[Convert.ToInt32(result_element[0]["element_id"])-1], Transporter.AgregatInputPlaces[PlayerPrefs.GetString($"{sys_building_name}Exit")].transform.position, Quaternion.identity);
-                }
-                is_reacted = true;
-            } else{ // если же он есть
-                reaction_state = "Реакция прошла успешно!";
-                foreach(Dictionary<string, string> element in result_element){
-                    if(Convert.ToInt32(element["element_id"]) == 0){
-                        continue;
-                    }
-                    
-                    if(exit == building_name){
-                        Instantiate(EP.elements_prefabs[Convert.ToInt32(element["element_id"])-1], OutputPlace.transform.position, Quaternion.identity);
-                    } else {
-                        Instantiate(EP.elements_prefabs[Convert.ToInt32(element["element_id"])-1], Transporter.AgregatInputPlaces[PlayerPrefs.GetString($"{sys_building_name}Exit")].transform.position, Quaternion.identity);
-                    }
-                }
-                is_reacted = true;
+        // если результата нет
+        if(Convert.ToInt32(result_element[0]["element_id"]) == temp_element_ids[0] && temp_action != "Аннигилировать, оставить основной элемент"){
+            reaction_state = "Нет такой реакции!"; // выставление соответствующего состояния агрегата
+            if(temp_exit == building_name){
+                Instantiate(EP.elements_prefabs[Convert.ToInt32(result_element[0]["element_id"])-1], OutputPlace.transform.position, Quaternion.identity);
+            } else {
+                Instantiate(EP.elements_prefabs[Convert.ToInt32(result_element[0]["element_id"])-1], Transporter.AgregatInputPlaces[temp_exit].transform.position, Quaternion.identity);
             }
-            InputElementsText.text = "";
-            DeleteAlgorithm();        
-            element_ids.Clear();
-            element_names.Clear();
-            temp_storage.Clear();
-            temp_element_ids.Clear();
-        } 
-    }
-
+            is_reacted = true;
+        } else{ // если же он есть
+            reaction_state = "Реакция прошла успешно!";
+            foreach(Dictionary<string, string> element in result_element){
+                if(Convert.ToInt32(element["element_id"]) == 0){
+                    continue;
+                }
+                if(temp_exit == building_name){
+                    Instantiate(EP.elements_prefabs[Convert.ToInt32(element["element_id"])-1], OutputPlace.transform.position, Quaternion.identity);
+                } else {
+                    Instantiate(EP.elements_prefabs[Convert.ToInt32(element["element_id"])-1], Transporter.AgregatInputPlaces[temp_exit].transform.position, Quaternion.identity);
+                }
+            }
+            is_reacted = true;
+        }
+        InputElementsText.text = "";
+        DeleteAlgorithm();        
+        element_ids.RemoveAt(temp_algorithm_id);
+        element_names.RemoveAt(temp_algorithm_id);
+        temp_storage.Clear();
+        temp_element_ids.Clear();
+        temp_action = null;
+        exits.RemoveAt(temp_algorithm_id);
+        temp_exit = null;
+        parameters.RemoveAt(temp_algorithm_id);
+    } 
 }
+
